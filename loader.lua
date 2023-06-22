8f6d9bcb2e28389a14c26aa8bf3052a57ab92d89
Reapss <pepesploitssss@gmail.com>
2023-06-22 19:14:32 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+print("loader executed")
+if game.PlaceId == 13028445510 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/main/shootwallsimulator.lua?token=GHSAT0AAAAAACDPYUPGLGRECBDP5LXGCSOGZEUQGIQ"))()
+else
+    print("Game Not Supported!")
+end

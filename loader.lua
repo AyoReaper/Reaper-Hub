e6d026e7bd9cc9a09b2ab0c32c199b2c20ab4949
Reapss <pepesploitssss@gmail.com>
2023-06-25 22:22:44 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,9 @@ if game.PlaceId == 13028445510 then
 elseif 
 game.PlaceId == 13127800756 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/main/armwrestlesim.lua"))()
+elseif 
+game.PlaceId == 13230182363 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/main/1moneyeveryclick.lua"))()
 else
     print("Game Not Supported!")
 end

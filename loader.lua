c15ffaf73f6cf808e09d12b81e70fb77587ec40a
Reapss <pepesploitssss@gmail.com>
2023-06-25 13:39:28 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,9 @@
 print("loader executed")
 if game.PlaceId == 13028445510 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/main/shootwallsimulator.lua"))()
+elseif 
+game.PlaceId == 13127800756 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/main/armwrestlesimulator.lua"))()
 else
     print("Game Not Supported!")
 end

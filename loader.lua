a5ae55972b1b9960ab75c85cda8e80ba6a5f3ddd
Reapss <pepesploitssss@gmail.com>
2023-06-27 22:37:20 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,6 +7,9 @@ game.PlaceId == 13127800756 then
 elseif 
 game.PlaceId == 13230182363 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/main/1moneyeveryclick.lua"))()
+elseif 
+game.PlaceId == 13284033561 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/main/fishingfrenzysimulator.lua"))()
 else
     print("Game Not Supported!")
 end

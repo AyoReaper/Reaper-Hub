361bae4b6a7d59e0c3ab05ddb2ed12a92eab2da9
Reapss <120023039+AyoReaper@users.noreply.github.com>
2024-10-22 22:12:16 +0200

    Update loader.lua
    + Added PETSGO! support

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 18901165922 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/animelostsimulator.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PETSGO!.lua"))()
 else
     print("Game Not Supported!")
 end

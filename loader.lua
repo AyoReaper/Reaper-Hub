13b78d366711ff39b3fdf65d094f1fdea8afcc8d
Reapss <120023039+AyoReaper@users.noreply.github.com>
2024-10-24 19:58:33 +0200

    Update loader.lua
    [+] PETS GO! Trading Plaza World Support

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 18901165922 then
+if game.PlaceId == 18901165922 or game.PlaceId == 19006211286 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PETSGO!.lua"))()
 else
     print("Game Not Supported!")

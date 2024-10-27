024e8928c5b123ebcd41602d0dbe8081cc9d40d9
Reapss <120023039+AyoReaper@users.noreply.github.com>
2024-10-27 02:11:02 +0200

    Update loader.lua
    fixed a retarded mistake

--- loader.lua
+++ loader.lua
@@ -1,7 +1,8 @@
 if game.PlaceId == 18901165922 or game.PlaceId == 19006211286 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PETSGO!.lua"))()
 else
-    if game.PlaceId == 8737899170 then
+if game.PlaceId == 8737899170 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/petsimulator99.lua"))()
+else
     print("Game Not Supported!")
 end

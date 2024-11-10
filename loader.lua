8489c9fa6ae4cbc766288f814e4f6d158719c7aa
Reapss <120023039+AyoReaper@users.noreply.github.com>
2024-11-10 04:46:58 +0100

    Update loader.lua
    [+] Event World Support

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 18901165922 or game.PlaceId == 19006211286 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PETSGO!.lua"))()
-elseif game.PlaceId == 8737899170 or game.PlaceId == 16498369169 or game.PlaceId == 15502339080 or game.PlaceId == 17503543197 then
+elseif game.PlaceId == 8737899170 or game.PlaceId == 16498369169 or game.PlaceId == 15502339080 or game.PlaceId == 17503543197 or game.PlaceId == 84810042603306 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PetSimulator99.lua"))()
 else
     print("Game Not Supported!")

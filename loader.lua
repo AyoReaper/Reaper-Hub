e56590e02b437f8c1a9892960e15630a89a19ba9
reaper. <120023039+AyoReaper@users.noreply.github.com>
2024-12-04 01:41:09 +0100

    Update loader.lua
    [+] The Strongest Battlegrounds Support

--- loader.lua
+++ loader.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 8737899170 or game.PlaceId == 16498369169 or game.PlaceId
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PetSimulator99.lua"))()
 elseif game.PlaceId == 16732694052 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/fisch.lua"))()
+    elseif game.PlaceId == 10449761463 or game.PlaceId == 12360882630 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/TheStrongestBattlegrounds.lua"))()
 else
     print("Game Not Supported!")
 end

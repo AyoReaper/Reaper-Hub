9fa74f6001ce1e60da65a21b5615d6afd05982f1
Keeron <keeron9@gmail.com>
2025-01-17 21:13:27 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 18901165922 or game.PlaceId == 19006211286 or game.PlaceId == 11619408261250 or game.PlaceId == 116194082612500 or game.PlaceId == 109487238458636 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PETSGO!.lua"))()
-elseif game.PlaceId == 8737899170 or game.PlaceId == 16498369169 or game.PlaceId == 15502339080 or game.PlaceId == 17503543197 or game.PlaceId == 84810042603306 then
+elseif game.PlaceId == 8737899170 or game.PlaceId == 16498369169 or game.PlaceId == 15502339080 or game.PlaceId == 17503543197 or game.PlaceId == 84810042603306 or game.PlaceId == 83876601629970 or game.PlaceId == 93168007533076 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/PetSimulator99.lua"))()
 elseif game.PlaceId == 16732694052 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Reaper-Hub/refs/heads/main/fisch.lua"))()
